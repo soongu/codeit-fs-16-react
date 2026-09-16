04ba114c48dbe5454507321561fa19f3d6323197
soongu <naong526@naver.com>
2026-09-16 10:05:52 +0900

    feat: FeedPage 컴포넌트 추가 및 App에서 라우팅 설정 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,10 @@
-import { useState } from 'react';
-import Stories from './components/Stories.jsx';
-import page from './components/FeedPage.module.scss';
-import stateStyles from './components/StatusMessage.module.scss';
-import FeedList from './components/FeedList.jsx';
-import CreateFeedModal from './components/CreateFeedModal.jsx';
-import UserSearch from './components/UserSearch.jsx';
-
-import { usePostsContext } from './contexts/PostsContext.jsx';
 import { Route, Routes } from 'react-router';
-
-const FeedScreen = () => {
-  
-  const [isCreateOpen, setIsCreateOpen] = useState(false);
-  const { error, addPost, selectUser } = usePostsContext();
-
-  return (
-    <main className={page.mainContent}>
-      <button
-        type='button'
-        onClick={() => setIsCreateOpen(true)}>
-        새 게시물
-      </button>
-
-      <UserSearch onSearch={selectUser} />
-
-      <Stories onSelect={selectUser} />
-
-      {error ? (
-        <p className={stateStyles.errorText}>{error}</p>
-      ) : (
-        <>
-          <FeedList />
-        </>
-      )}
-
-      {isCreateOpen && (
-        <CreateFeedModal
-          onClose={() => setIsCreateOpen(false)}
-          onCreate={addPost}
-        />
-      )}
-    </main>
-  );
-};
-
+import FeedPage from './pages/FeedPage.jsx';
 
 const App = () => { 
   return (
     <Routes>
-      <Route path="/" element={<FeedScreen />} />
-      <Route path="/home" element={<h2>홈 입니다.</h2>} />
-      <Route path="/login" element={<h2>로그인 입니다.</h2>} />
+      <Route path="/" element={<FeedPage />} />
     </Routes>
   );
 };
