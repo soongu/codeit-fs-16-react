59b37fad18f78468362b6579220eed10d9da6882
soongu <naong526@naver.com>
2026-09-16 11:09:13 +0900

    feat: InstagramLogo 및 Sidebar 컴포넌트 추가

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,42 @@
+// ~/instagram-react/src/components/Sidebar.jsx
+import { Link } from 'react-router';
+import { FaHouse } from 'react-icons/fa6';
+import InstagramLogo from './InstagramLogo.jsx';
+import styles from './Sidebar.module.scss';
+
+function Sidebar() {
+  return (
+    <nav className={styles.sidebar}>
+      <div className={styles.logoContainer}>
+        <Link
+          to='/'
+          className={styles.logo}>
+          <InstagramLogo />
+        </Link>
+      </div>
+
+      <div className={styles.menuContainer}>
+        <Link
+          to='/'
+          className={styles.menuItem}>
+          <FaHouse size={24} />
+          <span className={styles.menuText}>홈</span>
+        </Link>
+
+        <Link
+          to='/jaehoon'
+          className={styles.menuItem}>
+          <div className={styles.profileImage}>
+            <img
+              src='https://picsum.photos/seed/jaehoon/40/40'
+              alt='프로필'
+            />
+          </div>
+          <span className={styles.menuText}>프로필</span>
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Sidebar;
